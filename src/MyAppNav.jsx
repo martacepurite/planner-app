7de3fcaf3a66226df2534c4e890124d6c962eab7
marta <martacepurite@gmail.com>
2025-05-25 11:28:11 +0300

    navbar highlight active

--- src/MyAppNav.jsx
+++ src/MyAppNav.jsx
@@ -2,20 +2,30 @@ import { NavLink } from "react-router";
 
 export function MyAppNav() {
   return (
-    <nav className="flex flex-row bg-slate-300 gap-5 p-5 text-2xl shadow-md/40 items-center justify-center">
-      <NavLink className="hover:scale-110" to="planner-app/home" end>
+    <nav className="flex flex-row bg-slate-300 gap-7 p-5 text-2xl shadow-md/40 items-center justify-center">
+      <NavLink className={({ isActive }) =>
+    isActive ? "border-1 p-2 rounded-full" : "text-black hover:scale-110 p-2"
+  } to="planner-app/home" end>
         Home
       </NavLink>
-      <NavLink className="hover:scale-110" to="planner-app/tasks" end>
+      <NavLink className={({ isActive }) =>
+    isActive ? "border-1 p-2 rounded-full" : "text-black hover:scale-110 p-2"
+  } to="planner-app/tasks" end>
         My Tasks 
       </NavLink>
-      <NavLink className="hover:scale-110" to="planner-app/habits" end>
+      <NavLink className={({ isActive }) =>
+    isActive ? "border-1 p-2 rounded-full" : "text-black hover:scale-110 p-2"
+  } to="planner-app/habits" end>
         My Habits 
       </NavLink>
-      <NavLink className="hover:scale-110" to="planner-app/login" end>
+      <NavLink className={({ isActive }) =>
+    isActive ? "border-1 p-2 rounded-full" : "text-black hover:scale-110 p-2"
+    } to="planner-app/login" end>
        Login 
       </NavLink>
-      <NavLink className="hover:scale-110" to="planner-app/register" end>
+      <NavLink className={({ isActive }) =>
+    isActive ? "border-1 p-2 rounded-full" : "text-black hover:scale-110 p-2"
+  } to="planner-app/register" end>
       Register 
       </NavLink>
     </nav>
