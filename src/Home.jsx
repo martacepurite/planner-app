b9f6a283fb10937d671b1bd0d7a0bb5de6f890c4
marta <martacepurite@gmail.com>
2025-05-25 11:17:10 +0300

    center navbar

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <>
-      <div className="flex flex-row gap-20 p-20">
+      <div className="flex flex-row gap-20 p-20 items-center justify-center">
         <div className="flex basis-lg p-10 ">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
--- src/MyAppNav.jsx
+++ src/MyAppNav.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router";
 
 export function MyAppNav() {
   return (
-    <nav className="flex flex-row bg-slate-300 gap-5 p-5 text-2xl shadow-md/40 ">
+    <nav className="flex flex-row bg-slate-300 gap-5 p-5 text-2xl shadow-md/40 items-center justify-center">
       <NavLink className="hover:scale-110" to="planner-app/home" end>
         Home
       </NavLink>
